7947d824705ea7135959e5290ed070553ef12d96
Jaskaran <write2jaskaransingh@gmail.com>
2024-09-23 16:57:28 +0530

    update contact

--- src/components/Functions/contact.js
+++ src/components/Functions/contact.js
@@ -4,8 +4,17 @@ import { app } from "../../firebase/client";
 async function Contact(name, email, text) {
 
   const database = getDatabase(app);
-  let inputId = name + new Date().getTime();
-  await set(ref(database, "users/"+inputId), {
+  let inputId = name + new Date().toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+  await set(ref(database, "users/" + inputId + "(IST)"), {
     name: name,
     email: email,
     text: text,
